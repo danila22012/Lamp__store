4cbd7430f3ca75b20fa3915dccd881a5b5c5d423
danila22012 <danila222@ua.fm>
2020-12-17 23:37:45 +0200

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import  App  from "./Components/App/App";
-import styles from "./styles.scss";
-
-ReactDOM.render(<App/>,
-document.getElementById('root'))
