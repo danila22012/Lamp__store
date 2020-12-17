7d3b0f3fdf572f88853f62d05942af106b5fdda3
danila22012 <danila222@ua.fm>
2020-12-18 00:06:29 +0200

    once again

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './styles.scss'
 import { ReactComponent as NotFound404 } from "../../Assets/NotFound404.svg";
+import { NavLink } from "react-router-dom";
 
 export const NotFound = () => {
 
@@ -8,7 +9,7 @@ export const NotFound = () => {
         <div className="not-found">
             <NotFound404 className="not-found__img" />
             <p className="not-found__title">Sorry, we can't find this page</p>
-            <div className="not-found__btn"><button className="">HOME PAGE</button></div>
+            <div className="not-found__btn"><NavLink to='/home' >HOME PAGE</NavLink></div>
         </div>
 
     )
--- src/store.js
+++ src/store.js
@@ -1,14 +1,12 @@
-import { applyMiddleware, compose, createStore } from 'redux'
+import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
-
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { rootReducer } from "./rootReducer";
 
 export const store = createStore(
     rootReducer,
-    compose(
+    composeWithDevTools(
         applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
-
 );
